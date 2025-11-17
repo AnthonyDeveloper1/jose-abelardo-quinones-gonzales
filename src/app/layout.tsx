936cdc50f465874adf766f5db8683569a1d472f1
Edwing Antony De la Cruz Bautista <938266933asdgreasdasd@gmail.com>
2025-11-17 12:09:37 -0500

    Agrega Google Tag Manager en layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,24 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <meta name="google-site-verification" content="googlec49694f2a6707675.html" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KZJVL937');
+        ` }} />
+        {/* End Google Tag Manager */}
       </head>
-      <body className={inter.variable}>{children}</body>
+      <body className={inter.variable}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZJVL937" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   )
 }
